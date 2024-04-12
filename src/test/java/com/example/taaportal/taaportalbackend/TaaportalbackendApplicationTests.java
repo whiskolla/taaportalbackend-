fe515502f38e5b8300920456f9a327f82f557a17
whiskolla <bfoyb19@gmail.com>
2024-04-12 15:49:18 +0300

    start

---
+++ src/test/java/com/example/taaportal/taaportalbackend/TaaportalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taaportal.taaportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaaportalbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
